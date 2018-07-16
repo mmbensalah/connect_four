a05c2dee02afffde292b57fd1a2cf3f3d906fb95
mmbensalah <mmbensalah@gmail.com>
2018-07-16 13:26:49 -0600

    creates method for computer making a move and updating board, modifies runner file

--- lib/board.rb
+++ lib/board.rb
@@ -1,25 +1,23 @@
+require './lib/computer'
+
 class GameBoard
-  attr_accessor
+  attr_accessor :original_board
   def initialize
     @welcome_message = "Welcome to the game"
     @original_board =[["A", ".", ".", ".", ".", ".", "."],
-                    ["B", ".", ".", ".", ".", ".", "."],
-                    ["C", ".", ".", ".", ".", ".", "."],
-                    ["D", ".", ".", ".", ".", ".", "."],
-                    ["E", ".", ".", ".", ".", ".", "."],
-                    ["F", ".", ".", ".", ".", ".", "."],
-                    ["G", ".", ".", ".", ".", ".", "."]]
-    @output_board = @original_board.transpose
-
-    @output_board.each do |r|
-      puts r.each { |p| p }.join(" ")
-    end
+                      ["B", ".", ".", ".", ".", ".", "."],
+                      ["C", ".", ".", ".", ".", ".", "."],
+                      ["D", ".", ".", ".", ".", ".", "."],
+                      ["E", ".", ".", ".", ".", ".", "."],
+                      ["F", ".", ".", ".", ".", ".", "."],
+                      ["G", ".", ".", ".", ".", ".", "."]]
   end
 
   def display
+    @original_board.transpose.each do |r|
+      puts r.each { |p| p }.join(" ")
+    end
     puts @welcome_message
-    # puts @output_board
   end
-
 
 end
